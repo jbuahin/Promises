2019bb7a644196bd90697ab7f79604a0b171aabf
Joshua Amoa Buahin <jbuahin@gmail.com>
2017-02-28 18:49:03 -0700

    eigth

--- index.js
+++ index.js
@@ -7,39 +7,39 @@ return path.resolve(directoryPath,fileName);
 }
 
 
-exports.readFile  = function(filePath)
-{
-return new Promise(function(resolve,reject)
-{
-    fs.readFile(filePath, 'utf8', (err, data) => {
-      if (err){
-        reject(err);
-      }
-      resolve(data);
-    });
-  });
-}
+// exports.readFile  = function(filePath)
+// {
+// return new Promise(function(resolve,reject)
+// {
+    // fs.readFile(filePath, 'utf8', (err, data) => {
+      // if (err){
+        // reject(err);
+      // }
+      // resolve(data);
+    // });
+  // });
+// }
 
-exports.readDir = function(directoryPath){
-  return new Promise(function(resolve,reject){
-    fs.readdir(directoryPath, (err, files) => {
-      if (err){
-        reject(err);
-      }
-      resolve(files);
-    });
-  });
-}
+// exports.readDir = function(directoryPath){
+  // return new Promise(function(resolve,reject){
+    // fs.readdir(directoryPath, (err, files) => {
+      // if (err){
+        // reject(err);
+      // }
+      // resolve(files);
+    // });
+  // });
+// }
 
-exports.readDirFiles = function(directoryPath){
+// exports.readDirFiles = function(directoryPath){
     
-	var fileArray = [];
+	// var fileArray = [];
 	
-	return exports.readDir(directoryPath).then(function(files){
-		files.forEach(function(fileName){
-			fileArray.push(exports.readFile(exports.resolvedPath(directoryPath,fileName)));
-		}
-		return Promise.all(fileArray);
-		);
-	});
+	// return exports.readDir(directoryPath).then(function(files){
+		// files.forEach(function(fileName){
+			// fileArray.push(exports.readFile(exports.resolvedPath(directoryPath,fileName)));
+		// }
+		// return Promise.all(fileArray);
+		// );
+	// });
 }  
